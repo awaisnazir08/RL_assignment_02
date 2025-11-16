5ccd96ec9ac67c61cba94fe6d4c0999d1809a389
awaisnazir08 <owaisnazir2228@gmail.com>
2025-11-16 14:34:27 +0500

    config file for all the hyperparameters

---
+++ config.py
@@ -0,0 +1,8 @@
+# RL Agent Configuration
+
+MOUNTAIN_CAR_EPISODES = 500
+LUNAR_LANDER_EPISODES = 2000
+LEARNING_RATE = 0.1  # alpha (𝛼)
+DISCOUNT_FACTOR = 0.95  # gamma (𝛾)
+EXPLORATION_FACTOR = 0.2  # epsilon (𝜀)
+LAMBDA = 0.8  # lambda (𝜆)
